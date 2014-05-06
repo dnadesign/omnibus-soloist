f19aff1cb7856c72185d835bf80708d2ff0c2c79
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-05-06 12:38:20 +1200

    WIP - OS X 10.9 build

--- config/software/soloist.rb
+++ config/software/soloist.rb
@@ -1,10 +1,21 @@
-name "soloist"  
-version "1.0.1"
+name "soloist"
+default_version "1.0.1"
+
+# osx_build = (RUBY_PLATFORM =~ /darwin/)
 
 # the following dependencies come from 'omnibus-software'
-dependency "ruby"  
+dependency "ruby"
 dependency "rubygems"
+# dependency "plist" if osx_build
 
-build do  
+env = {
+  "ARCHFLAGS" => "-Wno-error=unused-command-line-argument-hard-error-in-future"
+}
+
+# if osx_build
+#   env["ARCHFLAGS"] = "-Wno-error=unused-command-line-argument-hard-error-in-future"
+# end
+
+build do
   gem "install #{name} -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}"
 end
